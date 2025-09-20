83cdfc1b95c86b9b2a85185fee10da87001100c8
Munna Dev <mkcodes.in@gmail.com>
2025-09-20 17:45:20 +0530

    localstorage with theme and try to make edit functionality

--- src/Components/Note.jsx
+++ src/Components/Note.jsx
@@ -1,16 +1,28 @@
 import React, { useState } from 'react'
 import { UseTheme } from '../Hooks/UseTheme'
 import { MdOutlinePushPin, MdPushPin } from 'react-icons/md'
-import { BiEdit, BiTrash } from 'react-icons/bi'
+import { BiCheck, BiEdit, BiTrash } from 'react-icons/bi'
 import UseFormData from '../Hooks/UseFormData';
 
 export default function Note() {
   const { theme } = UseTheme();
   const { state, dispatch } = UseFormData();
-  const [edit, setEdit] = useState(false);
-  const cardBg = theme ? 'bg-zinc-800' : 'bg-white'
-  const textColor = theme ? 'text-white' : 'text-zinc-900'
+  const [editText, setEditText] = useState(false);
+  const cardBg = theme ? 'bg-zinc-800' : 'bg-white';
+  const textColor = theme ? 'text-white' : 'text-zinc-900';
+  const [tilteText, setTitleText] = useState("");
+  const [tilteDescription, setTitleDescription] = useState("");
 
+  function handleEditText(e) {
+    setTitleText(e.target.value);
+    dispatch({ type: "EditNote", payload: tilteText })
+  }
+  console.log(tilteText)
+  function handleEditDes(e) {
+    setTitleDescription(e.target.value);
+    dispatch({ type: "EditNote", payload: tilteDescription })
+  }
+  console.log(tilteDescription)
   return (
     <div className={`min-h-screen py-22 transition-colors duration-300 ease-in-out ${theme ? "bg-gradient-to-b from-gray-900 to-gray-800" : "bg-gradient-to-b from-gray-100 to-gray-200"}`}>
       <div className='max-w-6xl mx-auto px-6 grid md:grid-cols-2 lg:grid-cols-3 justify-center'>
@@ -39,10 +51,10 @@ export default function Note() {
                     }
                   </button>
                   <div className='flex items-center gap-3'>
-                    <button 
-                    onClick={() => setEdit(!edit)}
-                    className='cursor-pointer p-1 rounded-full hover:bg-white/20 transition-all duration-200'>
-                      <BiEdit color='white' size={18} />
+                    <button
+                      onClick={() => setEditText(!editText)}
+                      className='cursor-pointer p-1 rounded-full hover:bg-white/20 transition-all duration-200'>
+                      {editText ? (<BiCheck size={18} />) : (<BiEdit color='white' size={18} />)}
                     </button>
                     <button
                       onClick={() => dispatch({ type: "DeleteNote", payload: item })}
@@ -54,9 +66,26 @@ export default function Note() {
 
                 {/* Content */}
                 <div className='pt-8 pb-4'>
-                  <h1 className='text-2xl font-bold mb-3 text-transparent bg-clip-text bg-gradient-to-r from-emerald-500 to-green-600'>{item.title}</h1>
+                  <h1 className='text-2xl font-bold mb-3 text-transparent bg-clip-text bg-gradient-to-r from-emerald-500 to-green-600'>{editText ?
+                    (
+                      <input
+                        className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-all duration-200"
+                        value={tilteText}
+                        onChange={(e) => handleEditText(e)}
+                        type="text" />
+                    ) :
+                    (item.title)}</h1>
                   <div className={`h-1 w-12 mb-4 rounded-full ${theme ? 'bg-emerald-700' : 'bg-emerald-400'}`}></div>
-                  <p className={`text-md leading-relaxed mb-6 ${theme ? 'text-gray-300' : 'text-gray-600'}`}>{item.description}</p>
+                  <p className={`text-md leading-relaxed mb-6 ${theme ? 'text-gray-300' : 'text-gray-600'}`}>
+                    {editText ?
+                      (
+                        <input
+                          className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-all duration-200"
+                          value={tilteDescription}
+                          onChange={(e) => handleEditDes(e)} type="text" />
+                      ) :
+                      (item.description)}
+                  </p>
                   {/* Date */}
                   <div className='flex justify-end'>
                     <span className={`text-xs px-3 py-1 rounded-full ${theme ? 'bg-zinc-700 text-gray-300' : 'bg-gray-100 text-gray-600'}`}>
--- src/Context/ThemeContext.jsx
+++ src/Context/ThemeContext.jsx
@@ -1,13 +1,23 @@
-import React, { createContext, useState } from 'react'
+import React, { createContext, useEffect, useState } from 'react'
 
 export const ThemeContext = createContext();
 
 export default function ThemeProvider({ children }) {
     const [theme, setTheme] = useState(false);
 
+    useEffect(() => {
+        const savedTheme = localStorage.getItem("theme");
+        if (savedTheme) {
+            setTheme(JSON.parse(savedTheme));
+        }
+    }, []);
+
     function handleTheme() {
-        setTheme(!theme);
+        const myTheme = !theme;
+        setTheme(myTheme);
+        localStorage.setItem("theme", JSON.stringify(myTheme));
     }
+
     return (
         <ThemeContext.Provider value={{ theme, handleTheme }}>
             {children}
